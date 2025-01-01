b50d7b6487793eead701242e9f9dc1757b7157b4
Stacey Chen <schen50@stuy.edu>
2025-01-01 14:59:20 -0500

    added print messages and changed char * to char[] for PP name

--- pipe_networking.c
+++ pipe_networking.c
@@ -16,12 +16,13 @@ int server_setup() {
     perror("failed to create wkp\n");
     exit(1);
   }
+  printf("Server created WKP, preparing to read...\n");
   from_client = open(WKP, O_RDONLY); // block until WKP can read
   if (from_client<0){
     printf("error opening WKP\n");
     exit(1);
   }
-  // read(f, &from_client, sizeof(int));
+  printf("Server removing WKP...\n");
   remove(WKP);
   return from_client;
 }
@@ -38,6 +39,7 @@ int server_setup() {
 int server_handshake(int *to_client) {
   int from_client = server_setup();
   *to_client = server_connect(from_client);
+  printf("End of server handshake.\n");
   return from_client;
 }
 
@@ -54,25 +56,32 @@ int server_handshake(int *to_client) {
 int client_handshake(int *to_server) {
   int from_server;
   int syn_ack;
-  char* name;
+  char name[200];
   sprintf(name, "%d", getpid());
+  printf("Client making private pipe...\n");
   int x = mkfifo(name, 0644); // make the private pipes
+  printf("Client opening WKP...\n");
   int f = open(WKP, O_WRONLY);
   if (f<0){
     printf("error opening WKP");
     exit(1);
   }
   *to_server = f; // set to fds for upstream pipe
+  printf("Client writing PP name to WKP...\n");
   write(f, name, sizeof(name)); // write PP to WKP
+  printf("Client opening PP...\n");
   from_server = open(name, O_RDONLY); // block until PP can read
   if (from_server<0){
     printf("error opening PP");
     exit(1);
   }
+  printf("Client removing PP...\n");
   remove(name);
+  printf("Client reading SYN_ACK...\n");
   read(from_server, &syn_ack, sizeof(int)); // reading syn_ack
+  printf("Client sending final ACK...\n");
   write(f, syn_ack+1, sizeof(int)); // send final ACK?
-
+  printf("End of client handshake.\n");
   return from_server;
 }
 
@@ -90,11 +99,15 @@ int server_connect(int from_client) {
   int pp = 0;
   int final_ack;
   int randNum = 13908; // change to random later
+  printf("Server reading PP PID...\n");
   read(from_client, &pp, sizeof(int)); //read the PP name/PID
-  char* name;
+  char name[200];
   sprintf(name, "%d", getpid());
+  printf("Server opening PP...\n");
   to_client = open(name, O_WRONLY); //open pp
+  printf("Server sending SYN_ACK...\n");
   write(to_client, randNum, sizeof(int)); // sending SYN_ACK
+  printf("Server reading final ACK...\n");
   read(from_client, &final_ack, sizeof(int)); // read final ACK?
 
   return to_client;
