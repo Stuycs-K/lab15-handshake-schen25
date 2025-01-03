e0ccec7149e2a555887db28f3fb8b2427a4f7902
STACEY_CHEN <schen50@stuy.edu>
2025-01-03 14:50:17 -0500

    edited basic server

--- basic_server.c
+++ basic_server.c
@@ -4,5 +4,10 @@ int main() {
   int to_client;
   int from_client;
 
+  srand(getpid());
+  int randNum = rand()%101;
+  printf("Sending %d to clients\n", randNum);
+
+
   from_client = server_handshake( &to_client );
 }
--- persistant_server.c
+++ persistant_server.c
@@ -4,5 +4,8 @@ int main() {
   int to_client;
   int from_client;
 
+  while (1){
+    
+  }
   from_client = server_handshake( &to_client );
 }
