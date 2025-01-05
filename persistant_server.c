98a7b2a4c8346fba668b1f948942ca4f589934dc
Stacey Chen <schen50@stuy.edu>
2025-01-05 18:33:37 -0500

    updated persistant so sends random number, still have to fix client not exiting

--- persistant_server.c
+++ persistant_server.c
@@ -3,9 +3,18 @@
 int main() {
   int to_client;
   int from_client;
+  int randNum;
 
   while (1){
     from_client = server_handshake( &to_client );
+    randNum = rand()%101;
+    printf("Sending %d to clients\n", randNum);
+    int w = write(to_client, &randNum, sizeof(int));
+    if (w<0){
+      printf("errno: %d %s", errno, strerror(errno));
+      exit(1);
+    }
+    sleep(1);
     close(to_client);
     close(from_client);
   }
