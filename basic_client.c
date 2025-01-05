46ea98342d679e59389850f61f98d46c75e40fca
Stacey Chen <schen50@stuy.edu>
2025-01-05 15:57:11 -0500

    updated basic server and basic client

--- basic_client.c
+++ basic_client.c
@@ -1,9 +1,16 @@
 #include "pipe_networking.h"
 
 int main() {
-
   int to_server;
   int from_server;
-
+  int randNum;
   from_server = client_handshake( &to_server );
+  while (1){
+    int r = read(from_server, &randNum, sizeof(int));
+    if (r<0){
+      printf("errno: %d %s", errno, strerror(errno));
+      exit(1);
+    }
+    printf("Received number: %d\n", randNum);
+  }
 }
--- basic_server.c
+++ basic_server.c
@@ -1,13 +1,29 @@
 #include "pipe_networking.h"
+#include <signal.h>
+
+// static void sighandler(int signo){
+//   if (signo==SIGINT){
+//
+//     kill(getpid(), SIGTERM);
+//   }
+// }
 
 int main() {
   int to_client;
   int from_client;
+  int randNum;
 
   srand(getpid());
-  int randNum = rand()%101;
-  printf("Sending %d to clients\n", randNum);
-
-
   from_client = server_handshake( &to_client );
+  while (1){
+    randNum = rand()%101;
+    printf("Sending %d to clients\n", randNum);
+    int w = write(to_client, &randNum, sizeof(int));
+    if (w<0){
+      printf("errno: %d %s", errno, strerror(errno));
+      exit(1);
+    }
+    sleep(1);
+  }
+
 }
