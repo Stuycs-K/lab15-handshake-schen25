5d322d634a67433b5bedcf70a82324ca9ede5007
Stacey Chen <schen50@stuy.edu>
2025-01-05 19:57:45 -0500

    fixed so client exits, basic client

--- basic_client.c
+++ basic_client.c
@@ -7,9 +7,8 @@ int main() {
   from_server = client_handshake( &to_server );
   while (1){
     int r = read(from_server, &randNum, sizeof(int));
-    if (r<0){
-      printf("errno: %d %s", errno, strerror(errno));
-      exit(1);
+    if (r<=0){
+      break;
     }
     printf("Received number: %d\n", randNum);
   }
